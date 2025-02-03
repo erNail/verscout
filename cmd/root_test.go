3394f5788166c7a78d799e34c23e0a77a6908574
Eric Nagel <nagel.eric.95@googlemail.com>
2025-02-03 10:00:50 +0100

    tests: Add test to validate that the `next` subcommand can be called

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -38,3 +38,14 @@ func TestRootCmdCallsLatestSubcommand(t *testing.T) {
 
 	require.NoError(t, err)
 }
+
+func TestRootCmdCallsNextSubcommand(t *testing.T) {
+	t.Parallel()
+
+	cmd := NewRootCmd()
+	cmd.SetArgs([]string{"next", "-h"})
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+}
